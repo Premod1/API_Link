7a5ee904880d8eec55382ca95cae5c1248b1e678
Premod1 <premodsuraweera1@gmail.com>
2022-08-28 10:56:42 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <PostList/>
+</template>
+
+<script>
+import PostList from './components/PostList.vue'
+export default {
+    name:'App',
+    components: {
+      PostList,
+    }
+}
+</script>
+
+<style>
+
+</style>
